8792348e68dd60018465761629096b9ea0d9f5a3
Ahmed Hany Saber Abo Zed Ahmed <130825620+AhmedHanySaber@users.noreply.github.com>
2024-12-23 04:59:59 +0200

    Task done

---
+++ DTO/DepartmentSummaryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace EmployeeApp.DTO
+{
+    public class DepartmentSummaryDto
+    {
+        public string Department { get; set; }
+        public int EmployeeCount { get; set; }
+        public double AverageAge { get; set; }
+    }
+}
---
+++ Data/EmployeeDbContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using EmployeeApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeApp.Data;
+
+public partial class EmployeeDbContext : DbContext
+{
+    public EmployeeDbContext()
+    {
+    }
+
+    public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseNpgsql("Host=dpg-ctk9hrtds78s73c6114g-a.frankfurt-postgres.render.com;Database=employeedb_rlkf;Username=ahmed;Password=9NjxHP0mp5ya8lNF6CNxWjgQnMZcSBd4");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("employee_pkey");
+
+            entity.ToTable("employee");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Age).HasColumnName("age");
+            entity.Property(e => e.Department)
+                .HasMaxLength(255)
+                .IsFixedLength()
+                .HasColumnName("department");
+            entity.Property(e => e.Isactive)
+                .HasDefaultValue(true)
+                .HasColumnName("isactive");
+            entity.Property(e => e.Name)
+                .HasMaxLength(255)
+                .IsFixedLength()
+                .HasColumnName("name");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
